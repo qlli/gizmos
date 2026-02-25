b981f4bc3b40d6a900b0a60c2192471299764213
quanlongli <quanlongli@tencent.com>
2026-02-25 11:48:56 +0800

    测试蓝盾流水线，UE5.7版本样例工程

---
+++ TestBKCI/UE5_7/Source/UE5_7/UE5_7.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE5_7.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_7, "UE5_7" );
